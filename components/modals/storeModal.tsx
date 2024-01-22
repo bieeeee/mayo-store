91624f978461c6d8ad1e3224fb5da4ce8ae5c268
hanbyeol kwon <janviertrois@naver.com>
2024-01-22 23:43:49 +0900

    feat: Implement onSubmit function using axios to post form values

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -2,6 +2,7 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { useModalStore } from "@/hooks/useModalStore";
@@ -9,6 +10,8 @@ import { Modal } from "@/components/ui/modal";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { NextResponse } from "next/server";
+import axios from "axios";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -16,6 +19,7 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
   const storeModal = useModalStore();
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -25,8 +29,15 @@ export const StoreModal = () => {
   })
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    //Create Store
-    console.log(values)
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/stores', values);
+      console.log(response.data)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -48,6 +59,7 @@ export const StoreModal = () => {
                     <FormLabel>Name</FormLabel>
                     <FormControl>
                       <Input
+                        disabled={loading}
                         placeholder="Mayo Store"
                         {...field}
                       />
@@ -57,8 +69,8 @@ export const StoreModal = () => {
                 )}
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                <Button type="submit">Continue</Button>
+                <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                <Button disabled={loading} type="submit">Continue</Button>
               </div>
             </form>
           </Form>
